e7a26ceb8f46b995291a2cfedb54ba4dfe058ae0
Ghislaine <glenox79@gmail.com>
2022-01-22 19:40:31 -0300

    feature: challenge day04 Rock-Paper-Scissors

---
+++ Day-04/Studies_exemples.py
@@ -0,0 +1,68 @@
+"""
+Study 1 - about random
+"""
+
+""" import random
+
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+random_number = random.randint(0, 1)
+print(random_number)
+
+random_float = random.random() * 5
+print(random_float)
+
+score = random.randint(1, 100)
+print(f"Your Score is {score}") """
+
+"""
+Study 2 - List
+
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia"]
+
+# change name
+# states_of_america[1] = "Pencilvania"
+
+# states_of_america.append("Angelaland")
+
+# states_of_america.extend(["Otherland", "Anotherland"])
+
+length_states = len(states_of_america)
+print(states_of_america[length_states - 1])
+"""
+
+# Study 3 - More List
+
+""" dirty_dozen = [
+    "Strawberries",
+    "Spinach",
+    "Kale",
+    "Nectarines",
+    "Apples",
+    "Grapes",
+    "Peaches",
+    "Cherries",
+    "Pears",
+    "Tomatoes",
+    "Celery",
+    "Potatoes",
+] """
+
+fruits = [
+    "Strawberries",
+    "Nectarines",
+    "Apples",
+    "Grapes"
+    "Peaches",
+    "Cherries",
+    "Pears",
+]
+
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
