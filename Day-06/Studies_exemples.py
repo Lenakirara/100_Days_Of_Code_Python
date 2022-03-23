ea00326e764971d739cf1015d99f668809b9e5e2
Ghislaine <glenox79@gmail.com>
2022-03-23 09:59:09 -0300

    feature: challenge day 6

---
+++ Day-06/Studies_exemples.py
@@ -0,0 +1,45 @@
+"""
+Functions & Karel
+
+def my_function():
+    # Do this
+    # Then do this
+    # finally do this
+
+Calling Functions
+my_function( )
+"""
+
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+print("=========================")
+
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
+
+
+"""
+For Loop
+
+for item in list_of_items:
+    # Do something
+
+for number in range(a, b):
+    print(number)
+ 
+-------------------------
+
+While Loop
+
+while something_is_true:
+    # Do this
+    # Then do this
+"""
+
+
